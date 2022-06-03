a9e50bdf4126100ecb5443818324e2dc0c3a87b2
dudubarbaro <eduardobarbaro.ifc@gmail.com>
2022-06-03 09:10:25 -0300

    master

---
+++ src/components/template/HeaderComp.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div class="menu-esquerdo">
+      <span> <RouterLink to="/"> Home </RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/times"> Times </RouterLink> </span>
+      <span>|</span>
+      <span> <RouterLink to="/jogadores"> Jogadores </RouterLink> </span>
+    </div>
+    <div class="menu-direito">
+      <span>Sair</span>
+    </div>
+  </header>
+</template>
+<style>
+header {
+  background-color: darkred;
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+  justify-content: space-between;
+}
+header span {
+  padding: 0 10px;
+}
+</style>
---
+++ src/components/template/RodapeComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>Rodapé</footer>
+</template>
+<style>
+footer {
+  background-color: darkred;
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+  justify-content: space-between;
+}
+</style>
